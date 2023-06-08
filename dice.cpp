4141183828fe04405a80811407e439dac5f87b03
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-06-08 16:17:13 +0530

    Update dice.cpp

--- dice.cpp
+++ dice.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     srand(time(0)); //seed rand
-    int num=rand() %(6-1+1)+1;
+    int num=rand() %(6-1+1)+1;   //6 is max value and 1 is mini value
 
     cout<<num;
 }
