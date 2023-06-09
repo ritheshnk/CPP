7570ad01166545b3605399a94e39e688e2299665
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-06-09 17:11:30 +0530

    Update reactangle_pattern.cpp

--- reactangle_pattern.cpp
+++ reactangle_pattern.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cout<<"enter the number of rows ";
+    int n,m;
+    cout<<"enter the number of rows and coloumns ";
     cin>>n;
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=4;j++)
+        for(int j=1;j<=m;j++)
         {
            cout<<"* ";
         }
